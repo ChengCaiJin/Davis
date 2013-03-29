fa1613989ae80a9d425b45edcbec154933d45df1
ChengCaiJin <goldendragon616@gmail.com>
2013-03-30 05:58:27 +0800

    this is panda
    panda

---
+++ HelloPanda-master/Emerald/Setting.h
@@ -0,0 +1,13 @@
+//
+//  Setting.h
+//  Emerald
+//
+//  Created by lion on 3/30/13.
+//  Copyright (c) 2013 coltboy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Setting : UIViewController
+
+@end
---
+++ HelloPanda-master/Emerald/Setting.m
@@ -0,0 +1,38 @@
+//
+//  Setting.m
+//  Emerald
+//
+//  Created by lion on 3/30/13.
+//  Copyright (c) 2013 coltboy. All rights reserved.
+//
+
+#import "Setting.h"
+
+@interface Setting ()
+
+@end
+
+@implementation Setting
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
